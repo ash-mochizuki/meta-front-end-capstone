bf5d47d9bcd06ec0b3678c4a0f4113b972432ba4
Ashton <38923493+ash-mochizuki@users.noreply.github.com>
2024-05-26 16:07:29 -0400

    final cleanup

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
-// import Header from './components/Header';
-// import BookingForm from './components/BookingForm';
-// import Booking from './components/Booking';
 import Main from './components/Main';
 import Menu from './components/Menu';
 import Footer from './components/Footer';
