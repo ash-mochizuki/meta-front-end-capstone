46d6942cb3af90e1645e6c1625f4d5a042ef9975
Ashton <38923493+ash-mochizuki@users.noreply.github.com>
2024-05-26 00:24:17 -0400

    added to booking

--- little-lemon/src/App.js
+++ little-lemon/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Nav from './components/Nav';
 import Header from './components/Header';
 import BookingForm from './components/BookingForm';
+import Booking from './components/Booking';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Nav/>
       <Header/>
       <BookingForm/>
+      <Booking/>
     </>
   );
 }
